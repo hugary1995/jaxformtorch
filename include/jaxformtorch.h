ec4991310710b4b2ed8d72d88c3c26c52a0bb4ec
Gary Hu <hugary1995@gmail.com>
2022-10-07 20:26:43 -0500

    For now, just a identity transformation

--- include/jaxformtorch.h
+++ include/jaxformtorch.h
@@ -1,1 +1,28 @@
 #include <torch/torch.h>
+
+#include <functional>
+
+namespace jxt
+{
+template <class F, class... Args>
+std::result_of_t<F && (Args && ...)>
+identity(F && f, Args &&... args)
+{
+  return f(args...);
+}
+
+template <typename Ret, typename... Args>
+class vmap
+{
+public:
+  vmap(const std::function<Ret(Args...)> & f)
+    : _f(f)
+  {
+  }
+
+  Ret operator()(Args &&... args) { return _f(args...); }
+
+private:
+  std::function<Ret(Args...)> _f;
+};
+}
--- tests/test.cxx
+++ tests/test.cxx
@@ -2,4 +2,14 @@
 
 #include <catch2/catch.hpp>
 
+#include <iostream>
+#include <cmath>
 #include "jaxformtorch.h"
+
+TEST_CASE("wrap a function", "[function]")
+{
+  std::function<double(double)> f = (double (*)(double)) & std::sin;
+  jxt::vmap<double, double> f_mapped(f);
+
+  REQUIRE(f_mapped(5.5) == Approx(std::sin(5.5)));
+}
