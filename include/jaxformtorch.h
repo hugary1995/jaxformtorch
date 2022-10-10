fece925893daec14e82302bb75976109e846f4e3
Gary Hu <thu@anl.gov>
2022-10-10 13:35:57 -0500

    jacrev works, but vmap(jacrev) does not

--- include/jaxformtorch.h
+++ include/jaxformtorch.h
@@ -2,14 +2,19 @@
 #include <ATen/Operators.h>
 #include <torch/torch.h>
 
-#include <functional>
-
 namespace jxt
 {
 template <typename F>
 class vmap
 {
 public:
+  vmap(F & f, const std::vector<int64_t> & in_dims = {0}, const int64_t out_dim = 0)
+    : _f(std::forward<F>(f)),
+      _in_dims(in_dims),
+      _out_dim(out_dim)
+  {
+  }
+
   vmap(F && f, const std::vector<int64_t> & in_dims = {0}, const int64_t out_dim = 0)
     : _f(std::forward<F>(f)),
       _in_dims(in_dims),
@@ -20,15 +25,23 @@ public:
   template <typename... Args>
   auto operator()(Args &&... args)
   {
+    // Dummy counter in fold expressions
+    int i = 0;
+
+    // Get the batch sizes
+    std::vector<int64_t> batch_sizes;
+    (batch_sizes.push_back(args.sizes()[_in_dims[i++]]), ...);
+    int64_t batch_size = get_batch_size(batch_sizes);
+
     // Get the current vmap level
-    auto level = at::impl::VmapMode::increment_nesting();
+    int64_t level = at::impl::VmapMode::increment_nesting();
 
     // Call the function on the batched input to get the batched output
-    int i = 0;
-    auto out_batched = _f(add_batch_dim(args, i++, level)...);
+    i = 0;
+    auto out_batched = _f(add_batch_dim(args, _in_dims[i++], level)...);
 
     // Remove the batch dimension from the batched output
-    auto out = _remove_batch_dim(out_batched, level, _batch_size, _out_dim);
+    auto out = _remove_batch_dim(out_batched, level, batch_size, _out_dim);
 
     // Finally, decrement the nesting level
     at::impl::VmapMode::decrement_nesting();
@@ -37,11 +50,23 @@ public:
   }
 
 private:
-  // Helper to extract the batch size and add the batch dimension to a tensor
-  torch::Tensor add_batch_dim(const torch::Tensor & tensor, int64_t i, int64_t level)
+  // Get and validate the batch size
+  int64_t get_batch_size(const std::vector<int64_t> & batch_sizes) const
   {
-    _batch_size = tensor.sizes()[i];
-    return i < 0 ? tensor : _add_batch_dim(tensor, _in_dims[i], level);
+    int64_t batch_size = batch_sizes[0];
+    const bool valid = std::all_of(batch_sizes.begin() + 1,
+                                   batch_sizes.end(),
+                                   [&](const int64_t & r) { return r == batch_size; });
+    if (!valid)
+      std::runtime_error("Batch sizes of arguments of a vmapped lambda are not consistent.");
+    return batch_size;
+  }
+
+  // Add batch dimension
+  torch::Tensor add_batch_dim(const torch::Tensor & tensor, int64_t dim, int64_t level) const
+  {
+    // If dim is negative then don't batch it
+    return dim < 0 ? tensor : _add_batch_dim(tensor, dim, level);
   }
 
   // Store this but let's hope that the compiler can optimize it away... who knows.
@@ -49,6 +74,45 @@ private:
 
   const std::vector<int64_t> _in_dims;
   const int64_t _out_dim;
-  int64_t _batch_size;
 };
+
+template <typename F>
+class vjp
+{
+public:
+  vjp(F & f, const torch::Tensor & arg)
+    : _arg(arg)
+  {
+    _arg.set_requires_grad(true);
+    _out = f(_arg);
+  }
+
+  vjp(F && f, const torch::Tensor & arg)
+    : _arg(arg)
+  {
+    _arg.set_requires_grad(true);
+    _out = f(_arg);
+  }
+
+  const torch::Tensor & value() const { return _out; }
+
+  torch::Tensor operator()(const torch::Tensor & cotangent)
+  {
+    return torch::autograd::grad({_out}, {_arg}, {cotangent}, true, true)[0];
+  }
+
+private:
+  torch::Tensor _arg;
+  torch::Tensor _out;
+};
+
+inline torch::Tensor
+jacrev(const torch::Tensor & out, const torch::Tensor & arg)
+{
+  const torch::Tensor cotangents = torch::eye(out.sizes()[0]);
+  torch::Tensor jac =
+      vmap([&](const torch::Tensor & cotangent)
+           { return torch::autograd::grad({out}, {arg}, {cotangent}, true, true)[0]; })(cotangents);
+  return jac;
+}
 }
--- tests/test.cxx
+++ tests/test.cxx
@@ -6,7 +6,7 @@
 #include <cmath>
 #include "jaxformtorch.h"
 
-TEST_CASE("vmap a lambda", "[function]")
+TEST_CASE("vmap a lambda", "[vmap]")
 {
   int64_t batch_size = 5;
   int64_t n = 3;
@@ -18,4 +18,62 @@ TEST_CASE("vmap a lambda", "[function]")
   torch::Tensor correct = torch::norm(x, 2, 1);
 
   REQUIRE(torch::allclose(norm_x, correct));
+}
+
+TEST_CASE("vmap matmul", "[vmap]")
+{
+  int64_t batch_size = 5;
+  torch::Tensor x = torch::randn({batch_size, 3});
+  torch::Tensor C = torch::randn({batch_size, 2, 3});
+
+  torch::Tensor y = jxt::vmap([](const torch::Tensor & C, const torch::Tensor & x)
+                              { return torch::matmul(C, x); },
+                              {0, 0})(C, x);
+
+  REQUIRE(torch::allclose(y, torch::matmul(C, x.unsqueeze(2)).squeeze(2)));
+}
+
+TEST_CASE("vjp", "[vjp]")
+{
+  torch::Tensor x = torch::randn({3});
+  torch::Tensor C = torch::randn({2, 3});
+  torch::Tensor cotangents = torch::eye(2);
+
+  auto f = [&](const torch::Tensor & x) { return torch::matmul(C, x); };
+
+  torch::Tensor jac_row1 = jxt::vjp(f, x)(cotangents.index({0}));
+  torch::Tensor jac_row2 = jxt::vjp(f, x)(cotangents.index({1}));
+
+  REQUIRE(torch::allclose(jac_row1, C.index({0})));
+  REQUIRE(torch::allclose(jac_row2, C.index({1})));
+}
+
+TEST_CASE("jacrev", "[jacrev]")
+{
+  torch::Tensor x = torch::randn({3});
+  x.set_requires_grad(true);
+  torch::Tensor C = torch::randn({2, 3});
+
+  torch::Tensor y = torch::matmul(C, x);
+  torch::Tensor jac = jxt::jacrev(y, x);
+
+  REQUIRE(torch::allclose(jac, C));
+}
+
+TEST_CASE("vmap jacrev", "[vmap][jacrev]")
+{
+  int64_t batch_size = 5;
+  torch::Tensor x = torch::randn({batch_size, 3});
+  torch::Tensor C = torch::randn({batch_size, 2, 3});
+
+  torch::Tensor jac = jxt::vmap(
+      [&](const torch::Tensor & C, const torch::Tensor & x)
+      {
+        torch::Tensor x_primal = x.clone().set_requires_grad(true);
+        torch::Tensor y = torch::matmul(C, x);
+        return jxt::jacrev(y, x);
+      },
+      {0, 0})(C, x);
+
+  std::cout << jac << std::endl;
 }
